490af0b352d1367dd93f4329dba705fd9b0e3441
nicogamer6 <nicolascrypto@yahoo.com>
2018-11-11 15:28:50 +0100

    Début matrice ROC

---
+++ include/matriceROC.h
@@ -0,0 +1,11 @@
+#ifndef __MATRICEROC_H__
+#define __MATRICEROC_H__
+
+#include "nrutil.h"
+#include "nrdef.h"
+
+void matriceROC(uint8 **Et, uint8 ** Itverite, long nrl, long nrh, long ncl, long nch);
+
+    
+#endif // __MATRICEROC_H__
+
---
+++ src/matriceROC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "nrutil.h"
+#include "nrdef.h"
+#include "string.h"
+#include "test_morpho.h"
+#include "morpho.h"
+#include "mouvement.h"
+#include "matriceROC.h"
+
+
+
+void matriceROC(uint8 **Et, uint8 ** Itverite, long nrl, long nrh, long ncl, long nch){
+    
+}
